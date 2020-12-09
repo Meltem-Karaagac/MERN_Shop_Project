5df9fdd527e4e39d463815eb5127e0c9db4ed433
meltem <meltemkaraagac19@gmail.com>
2020-12-09 00:38:12 -0500

    add

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import Footer from './components/Footer'
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path='/' exact component={HomeScreen} />
+          <Route path='/Product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
