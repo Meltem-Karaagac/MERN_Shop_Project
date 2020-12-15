cdc9c2e669214fa87095b73328528e18cf8db6c3
meltem <meltemkaraagac19@gmail.com>
2020-12-14 21:34:57 -0500

    add

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import {Container,Row,Col} from 'react-bootstrap'
 
 const Footer = () => {
     return (
         <footer>
-            <Container>
-                <Row>
-                    <Col className="text-center py-3">
-                        Copyright &copy; ProShop
+          <Container>
+              <Row>
+                  <Col className="text-center py-3">
+                      Copyright &copy; ProShop
                   </Col>
-                </Row>
-            </Container>
+              </Row>
+          </Container>
         </footer>
     )
 }
