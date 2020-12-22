256ac0136c43817bf37249e9f9c4c66b6bfb1198
meltem <meltemkaraagac19@gmail.com>
2020-12-22 09:32:59 -0500

    add

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 const express = require('express')
-const path =require('path')
-const colors=require('colors')
+const path = require('path')
+const colors = require('colors')
 const dotenv = require('dotenv')
-const morgan=require('morgan')
-const errorMiddleware= require('./middleware/errorMiddleware')
+const morgan = require('morgan')
+const errorMiddleware = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 dotenv.config()
 
-const productRoutes=require('./routes/productRoutes')
-const userRoutes=require('./routes/userRoutes')
-const orderRoutes =require('./routes/orderRoutes')
-const uploadRoutes =require('./routes/uploadRoutes')
+const productRoutes = require('./routes/productRoutes')
+const userRoutes = require('./routes/userRoutes')
+const orderRoutes = require('./routes/orderRoutes')
+const uploadRoutes = require('./routes/uploadRoutes')
 
 
 connectDB()
@@ -31,7 +31,7 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
-// const __dirname = path.resolve()
+__dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
@@ -52,4 +52,4 @@ app.use(errorMiddleware.errorHandler)
 const PORT = process.env.PORT || 5000
 
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
