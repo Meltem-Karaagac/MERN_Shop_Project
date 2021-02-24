157c1738856e7bb57511850916604ea7923003d2
meltem <meltemkaraagac19@gmail.com>
2021-02-24 18:38:45 -0500

    updated

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -19,7 +19,7 @@ const SearchBox = ({ history }) => {
         type='text'
         name='q'
         onChange={(e) => setKeyword(e.target.value)}
-        placeholder='Search Products...'
+        placeholder='Search Products....'
         className='mr-sm-2 ml-sm-5 search-form'
       ></Form.Control>
       <Button type='submit' className='p-2 search-btn'>
