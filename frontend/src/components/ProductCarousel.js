a2fa63a860c5c81930da2da55630a3f34745fb38
meltem <meltemkaraagac19@gmail.com>
2021-02-24 21:42:28 -0500

    updated

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,4 +1,4 @@
-  
+
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Carousel, Image } from 'react-bootstrap'
@@ -22,21 +22,21 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant='danger'>{error}</Message>
   ) : (
-    <Carousel pause='hover'  className='carousel'>
-      {products.map((product) => (
-        <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name}   fluid />
-            <Carousel.Caption className='carousel-caption'>
-              <h4>
-                {product.name} (${product.price})
+        <Carousel pause='hover' className='carousel'>
+          {products.map((product) => (
+            <Carousel.Item key={product._id}>
+              <Link to={`/product/${product._id}`}>
+                <Image src={product.image} alt={product.name} fluid />
+                <Carousel.Caption className='carousel-caption carousel-text'>
+                  <h4>
+                    {product.name} (${product.price})
               </h4>
-            </Carousel.Caption>
-          </Link>
-        </Carousel.Item>
-      ))}
-    </Carousel>
-  )
+                </Carousel.Caption>
+              </Link>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      )
 }
 
 export default ProductCarousel
