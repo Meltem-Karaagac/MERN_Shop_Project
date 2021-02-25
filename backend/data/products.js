971ceac02241cbde7a0289da511996c53f471a1b
meltem <meltemkaraagac19@gmail.com>
2021-02-24 23:10:36 -0500

    updated

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,86 +1,12 @@
-// const products = [
-//   {
-//     name:  'iPhone 12 pro',
-//     image: '/images/1.png',
-//     description:
-//     'Industry‑leading IP68 water resistance,Ceramic Shield, tougher than any smartphone glass',
-//     brand: 'Apple',
-//     category: 'Electronics',
-//     price: 999.00,
-//     countInStock: 10,
-//     rating: 4.5,
-//     numReviews: 12,
-//   },
-//   {
-//     name: 'iPhone 11 Pro 256GB Memory',
-//     image: '/images/phone.jpg',
-//     description:
-//       'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
-//     brand: 'Apple',
-//     category: 'Electronics',
-//     price: 599.99,
-//     countInStock: 7,
-//     rating: 4.0,
-//     numReviews: 8,
-//   },
-//   {
-//     name: 'Cannon EOS 80D DSLR Camera',
-//     image: '/images/camera.jpg',
-//     description:
-//       'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
-//     brand: 'Cannon',
-//     category: 'Electronics',
-//     price: 929.99,
-//     countInStock: 5,
-//     rating: 3,
-//     numReviews: 12,
-//   },
-//   {
-//     name: 'Sony Playstation 4 Pro White Version',
-//     image: '/images/playstation.jpg',
-//     description:
-//       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
-//     brand: 'Sony',
-//     category: 'Electronics',
-//     price: 399.99,
-//     countInStock: 11,
-//     rating: 5,
-//     numReviews: 12,
-//   },
-//   {
-//     name: 'Logitech G-Series Gaming Mouse',
-//     image: '/images/mouse.jpg',
-//     description:
-//       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
-//     brand: 'Logitech',
-//     category: 'Electronics',
-//     price: 49.99,
-//     countInStock: 7,
-//     rating: 3.5,
-//     numReviews: 10,
-//   },
-//   {
-//     name: 'Amazon Echo Dot 3rd Generation',
-//     image: '/images/alexa.jpg',
-//     description:
-//       'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
-//     brand: 'Amazon',
-//     category: 'Electronics',
-//     price: 29.99,
-//     countInStock: 0,
-//     rating: 4,
-//     numReviews: 12,
-//   },
-// ]
 
-// module.exports = products
+
 
 const products = [
   {
     name: 'BRACELET',
     image: '/images/BRACELET_1.png',
     description:
-      'Industry‑leading IP68 water resistance,Ceramic Shield, tougher than any smartphone glass',
+      '18K white gold diamonds',
     brand: 'CHANEL',
     category: 'EARRINGS',
     price: 94799,
@@ -92,13 +18,13 @@ const products = [
     name: 'CAMELIA',
     image: '/images/CAMELIA.png',
     description:
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      '18K white gold diamonds',
     brand: 'CHANEL',
     category: 'EARRINGS',
     price: 99988,
     countInStock: 7,
-    rating: 4.0,
-    numReviews: 8,
+    rating: 1.0,
+    numReviews: 0,
   },
   {
     name: 'BOUTONDE EARRINGS',
@@ -109,7 +35,7 @@ const products = [
     category: 'EARRINGS',
     price: 9799,
     countInStock: 5,
-    rating: 3,
+    rating: 5,
     numReviews: 12,
   },
   {
@@ -164,7 +90,7 @@ const products = [
     name: 'CHANEL GOLD WRISTBAND',
     image: '/images/BRACELET.png',
     description:
-      '18K white gold, diamonds, cultured pearls',
+      'Metal & Strass Gold & Crystal',
     brand: 'CHANEL',
     category: 'WRISTBAND',
     price: 13499,
@@ -176,7 +102,7 @@ const products = [
     name: 'CHANEL WRISTBAND',
     image: '/images/CUFF.png',
     description:
-      '18K white gold, diamonds, cultured pearls',
+      'Metal & Strass Gold & Crystal',
     brand: 'CHANEL',
     category: 'WRISTBAND',
     price: 15499,
